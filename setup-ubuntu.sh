f0307785688ea58fc69a9015e6ef545805c22c5e
Uduru0522 <uduru0522@gmail.com>
2023-08-30 14:27:44 +0900

    Add echo to activate starship shell

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install starship terminal
 apt-get install curl
-curl -sS https://starship.rs/install.sh | sh;
+curl -sS https://starship.rs/install.sh | sh
+echo "eval \"$(starship init bash)\"" >> ~/.bashrc
 
 # Get Nerdfont & Normal font for DejaVu
 mkdir DejaVuNerdFontMono && cd DejaVuNerdFontMono
