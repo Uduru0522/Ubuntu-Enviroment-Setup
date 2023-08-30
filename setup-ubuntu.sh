a890d170b75be3e241b29075350902a88a4316aa
Uduru0522 <uduru0522@gmail.com>
2023-08-30 14:10:52 +0900

    Add set font for terminal, rename script name

---
+++ setup-ubuntu.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Install starship terminal
+apt-get install curl
+curl -sS https://starship.rs/install.sh | sh;
+
+# Get Nerdfont & Normal font for DejaVu
+mkdir DejaVuNerdFontMono && cd DejaVuNerdFontMono
+TTF_URLS=($(<$SCRIPT_DIR/fonts-url.txt))
+for url in "${TTF_URLS[@]}"
+do
+	curl -fLO $url
+done
+
+cd ..
+mv ./DejaVuNerdFontMono/ /usr/local/share/fonts
+
+FONT_NAME=fc-scan --format "%{fullname[$(( $(sed -E 's/^(.*)en.*/\1/;s/[^,]//g' <<<"$(fc-scan --format "%{fullnamelang}\n" /usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-Regular.ttf)" | wc -c) -1 ))]}\n" /usr/local/share/fonts/
+
+# Set font for Ubuntu console
+CONSOLE_PROFILE_UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \') 
+gsetting set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${CONSOLE_PROFILE_UUID}/ use-system-font false
+gsetting set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${CONSOLE_PROFILE_UUID}/ font 'DejaVuSansM Nerd Font Mono 16'
